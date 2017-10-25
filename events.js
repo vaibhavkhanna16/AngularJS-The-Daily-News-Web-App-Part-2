3f0bff3dc90158fe5dc051d930acea298d4b0b5e
Vaibhav Khanna <32571472+vaibhavkhanna16@users.noreply.github.com>
2017-10-25 21:24:39 +0530

    This App is developed by using AngularJS $http as a service which fetches the required data. This time the user sees the respective image of the news as well, also I made the links clickable, improved the UI  and a much better app than developed in part-1 using JQuery for AJAX calls. Also I used the CSS Pre-Processor LESS for styling.

---
+++ events.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $("#breadcrumb1").click(function(){
+    alert("Go ahead and watch general news!.");
+    });
+
+  $("#breadcrumb2").click(function(){
+    alert("Go ahead and watch Business news!.");
+    });
+
+  $("#breadcrumb3").click(function(){
+    alert("Go ahead and watch National Geographic news!.");
+    });
+
+  $("#breadcrumb4").click(function(){
+    alert("Go ahead and watch Sports news!.");
+    });
+
+  $("#breadcrumb5").click(function(){
+    alert("Go ahead and watch Technical news!.");
+    });
+
+});
---
+++ geographicnews.js
@@ -0,0 +1,36 @@
+(function(){
+
+  'use strict';
+
+  angular.module('MyNewsApp',[])
+  .controller('NewsController',NewsController)
+  .service('NewsService',NewsService);
+
+  NewsController.$inject=['NewsService'];
+
+  function NewsController(NewsService){
+    var news=this;
+
+    var promise = NewsService.getData();
+    promise.then(function(response){
+      news.newsData = response.data.articles;
+    });
+  }
+
+  NewsService.$inject = ['$http'];
+  function NewsService($http){
+    var service=this;
+
+    service.getData = function(){
+
+      var response = $http({
+
+        method:'GET',
+        url: 'https://newsapi.org/v1/articles?source=national-geographic&sortBy=top&apiKey=' + "d482a9751e764532b25ff420b503e2e7"
+
+      });
+
+      return response;
+    };
+  }
+})();
